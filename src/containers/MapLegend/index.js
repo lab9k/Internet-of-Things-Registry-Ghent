d4fa4aee3ce2019c443bb5df35fbfe3f49c011df
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-09-01 15:50:24 +0200

    save before eject

--- src/containers/MapLegend/index.js
+++ src/containers/MapLegend/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+import { useTranslation } from 'react-i18next';
 
 import { ReactComponent as CollapseIcon } from '../../icons/icon-arrow-down.svg'
 import { ReactComponent as ExpandIcon } from '../../icons/icon-arrow-up.svg'
@@ -13,51 +14,37 @@ function MapLegend(props) {
   const [isLegendVisible, setVisible] = useState(window.innerWidth > 576)
   const [t] = useTranslation()
 
-  render() {
-    const checkboxList = Object.entries(
-      this.props.categories
-    ).map(([id, category]) => getCategoryDivs(
-      category,
-      id,
-      this.props.onCategoryToggle,
-      this.props.onTypeToggle
-    ))
-    const { isLegendVisible } = this.state
-    const ariaLabel = `${formatMessage(messages.title)}, ${
-      isLegendVisible
-        ? formatMessage(messages.hide)
-        : formatMessage(messages.show)
-    }`
-    return (
-      <section
-        id="map-legend"
-        aria-label={ariaLabel}
-        className={`
+  const checkboxList = Object.entries(
+    props.categories
+  ).map(([id, category]) => getCategoryDivs(
+    category,
+    id,
+    props.onCategoryToggle,
+    props.onTypeToggle
+  ))
+  return (
+    <section
+      id="map-legend"
+      className={`
           map-legend
           map-legend--${isLegendVisible ? 'expanded' : 'collapsed'}
         `}
+    >
+      <button
+        type="button"
+        className="map-legend__header"
+        onClick={() => setVisible(!isLegendVisible)}
       >
-        <button
-          type="button"
-          className="map-legend__header"
-          onClick={() => this.setState({ isLegendVisible: !isLegendVisible })}
-          title={
-            isLegendVisible
-              ? formatMessage(messages.hide)
-              : formatMessage(messages.show)
-          }
-        >
-          <MapLayersIcon className="map-legend__header-icon" />
-          <h4 className="map-legend__header-title" aria-hidden="true">
-            {formatMessage(messages.devices)}
-          </h4>
-          <CollapseIcon className="map-legend__header-icon map-legend__header-icon--expanded" />
-          <ExpandIcon className="map-legend__header-icon map-legend__header-icon--collapsed" />
-        </button>
-        <div className="map-legend__body">{checkboxList}</div>
-      </section>
-    )
-  }
+        <MapLayersIcon className="map-legend__header-icon" />
+        <h4 className="map-legend__header-title" aria-hidden="true">
+          {t('mapLegend.devices')}
+        </h4>
+        <CollapseIcon className="map-legend__header-icon map-legend__header-icon--expanded" />
+        <ExpandIcon className="map-legend__header-icon map-legend__header-icon--collapsed" />
+      </button>
+      <div className="map-legend__body">{checkboxList}</div>
+    </section>
+  )
 }
 
 MapLegend.propTypes = {
--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,8 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import Backend from 'i18next-http-backend';
+import Backend from 'i18next-chained-backend';
+import LocalStorageBackend from 'i18next-localstorage-backend'
+import HttpApi from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 
 i18n
@@ -16,6 +18,17 @@ i18n
     },
     interpolation: {
       escapeValue: false
+    },
+    backend: {
+      backends: [
+        LocalStorageBackend,
+        HttpApi
+      ],
+      backendOptions: [{
+        store: window.localStorage
+      }, {
+        loadPath: 'locales/{{lng}}.json'
+      }]
     }
   })
 
