827ee99306bdcd88c5961a4da2d5c0e38a34ed36
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-09-01 16:33:10 +0200

    fixed a bug caused by https://github.com/facebook/react/issues/14022 that still exists in React

--- src/containers/MarkerCluster/index.js
+++ src/containers/MarkerCluster/index.js
@@ -4,8 +4,8 @@ import 'leaflet.markercluster/dist/leaflet.markercluster'
 import 'leaflet.markercluster/dist/MarkerCluster.css'
 import 'leaflet.markercluster/dist/MarkerCluster.Default.css'
 import { useLeaflet } from 'react-leaflet'
-import { renderToString } from 'react-dom/server';
-import LMarker, { createIcon } from '../LeafletMarker'
+import { renderToStaticMarkup } from 'react-dom/server'
+import { createIcon } from '../LeafletMarker'
 
 const mcg = L.markerClusterGroup()
 
@@ -27,12 +27,8 @@ const MarkerCluster = ({ markers }) => {
         }
       )
         .addTo(mcg)
-        .bindPopup(renderToString(LMarker(LMarkerObject.props.device)))
+        .bindPopup(renderToStaticMarkup(LMarkerObject))
     })
-
-    // optionally center the map around the markers
-    // map.fitBounds(mcg.getBounds());
-    // // add the marker cluster group to the map
     map.addLayer(mcg)
   }, [markers, map])
 
