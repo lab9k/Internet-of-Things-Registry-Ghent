4c9dd667f9433fdc518511f70405800fe69b2f61
Sander Machado <sander.castanheiramachado@student.hogent.be>
2020-09-03 16:38:13 +0200

    added some more translation

--- src/containers/AboutPage/index.js
+++ src/containers/AboutPage/index.js
@@ -4,15 +4,13 @@ import logo from '../../icons/logo--cyan.svg'
 export default function AboutPage() {
   return (
     <div className="container">
-      <row>
-        <img  alt="logo stad gent" src={logo} />
-        <div>
+      <div className="row">
+        <img className="col-1" alt="logo stad gent" src={logo} />
+        <div className="col-11">
           <h1>This is the about page</h1>
           <p>this is the about page content</p>
         </div>
-      </row>
+      </div>
     </div>
-
-
   )
 }
